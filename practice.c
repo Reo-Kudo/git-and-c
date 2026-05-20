30b674dc3f32a60420044350c5a1cb1117e1f5e9
Reo-Kudo <hip002456@gmail.com>
2026-05-20 14:43:27 +0900

    定数の四則演算を printf するソースコード

--- practice.c
+++ practice.c
@@ -2,15 +2,22 @@
 
 int main() {
 
-	printf("12345\n");
-	printf("ABCDabcd\n");
-	printf("‚ ‚¢‚¤‚¦‚¨\n");
-	printf("Š¿Žš\n");
+	printf("1+1=%d\n", 2);
+	printf("5-3=%d\n",5-3);
+	printf("3*2=%d\n",3*2);
+	printf("5/2=%d\n", 5/2);
+	printf("5%%2=%d\n", 5 % 2);
 
-	printf("%d\n", 5);
-	printf("%d\n", 5 + 3);
-	printf("%lf\n", 5.0 / 3.0);
+	printf("1.5+1.4=%d\n", 1.5+1.4);
+	printf("1.5+1.4=%f\n", 1.5+1.4);
+
+	printf("3+3=%d\n", 3+3);
+	printf("3+3=%3d\n", 3+3);
+	printf("1.5+1.4=%.2f\n", 1.5 + 1.4);
+	printf("1.5+1.4=%2.2f\n", 1.5 + 1.4);
+	printf("1.5+1.4=%4.2f\n", 1.5 + 1.4);
+	printf("1.5+1.4=%5.2f\n", 1.5 + 1.4);
+	printf("1.5+1.4=%6.2f\n", 1.5 + 1.4);
 
 	return 0;
-
 }
