6ba0e5529d0b71b32f36eccacbaacedc18b9805d
Reo-Kudo <hip002456@gmail.com>
2026-05-20 16:49:32 +0900

    キー入力を検出するソースコードに変更

--- practice.c
+++ practice.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
+#include <conio.h>
+
+void functionS();
 
 int main() {
 
-	int number;
-	double dnumber;
+	int key = 0, key2 = 0, a=0;
 
-	number = 3;
-	dnumber = 3.14;
+	a = 'q';
+	printf("%d", a);
 
-	printf("1:number=%d\n", number);
-	printf("1:dnumber=%f\n", dnumber);
+	do
+	{
+		if (_kbhit()) {
+			key = _getch();
+			//printf("%x\n", key)
 
-	number = number + 2;
-	printf("2:number=%d\n", number);
 
-	number = 10 / 5;
-	printf("3:number=%d\n", number*2);
-	printf("4:number=%d\n", number);
+			switch (key) {
+			case 0xe0: printf("矢印の");
+				key2 = _getch();
+				switch (key2) {
+				case 0x4b:	printf("左\n");	break;
+				case 0x4d:	printf("右\n");	break;
+				case 0x48:	printf("上\n");	break;
+				case 0x50:	printf("下\n");	break;
+				}break;
+			case 'a':	printf("aです\n");	break;
+			case 's':	functionS();	break;
+			case 'd':	printf("dです\n");	break;
+			case 'A':	printf("Aです\n");	break;
+			case 'q':	printf("qで終了\n");	break;
+			default:	printf("そのキーは使用できません。[q]キーを押すと終了\n");
 
+			}
+		}
+	} while (key!='q');
+	printf("お疲れ様でした。\n\n");
 	return 0;
+}
+
+void functionS() {
+	printf("\n");
+	printf("エスです\n");
+	printf("\n");
+	return;
 }
