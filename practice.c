6d6a1fcca28c6148e345b37145fe44a472e21e8d
Reo-Kudo <hip002456@gmail.com>
2026-05-20 09:59:38 +0900

    開発環境構築直後のコミット

---
+++ practice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("12345\n");
+	printf("ABCDabcd\n");
+	printf("‚ ‚¢‚¤‚¦‚¨\n");
+	printf("Š¿Žš\n");
+
+	printf("%d\n", 5);
+	printf("%d\n", 5 + 3);
+	printf("%lf\n", 5.0 / 3.0);
+
+	return 0;
+
+}
